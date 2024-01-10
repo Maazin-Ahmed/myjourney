8a31407c082b6e52118ebed1304893af97f15d09
Maazin-Ahmed <144589820+Maazin-Ahmed@users.noreply.github.com>
2024-01-10 19:47:00 +0530

    Update add.cpp
    Addition of 2 Numbers

--- code.cpp
+++ code.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+  int x, y, sum;
+  cout<<"Enter the First Number : ";
+  cin>>x;
+  cout<<"Enter the Second Number : ";
+  cin>>y;
+  sum= x + y;
+  cout<<"The Sum of the 2 Numbers is :" <<sum;
+
+  return 0;
+}
+
 
